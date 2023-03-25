1859315d70dccb21d8b9d437c7d6d25cbe8b5008
unknown <biancapeleao@gmail.com>
2023-03-25 20:24:44 -0300

    inicio js do aluno

--- js/aluno.js
+++ js/aluno.js
@@ -2,15 +2,55 @@
 
 const containerAluno = document.getElementById('containerAluno')
 
+
 const criandoContainerAluno = () => {
     const img = document.createElement('img')
     img.src = './img/image 1 (1).png'
     const span = document.createElement('span')
     span.innerHTML = 'José Matheus da Silva Miranda'
-    containerAluno.append(img,span)
+    containerAluno.append(img, span)
 }
 
 criandoContainerAluno()
+
+
+
+
+const criandoGrafico = () => {
+    const grafico = document.getElementById('grafico');
+    const notas = [90, 30, 50, 90, 20, 50];
+    const materias = ['SOP', 'LIMA', 'PWBE', 'PWFE', 'HARE', 'SOP'];
+
+    for (let i = 0; i < notas.length; i++) {
+    
+        const areaNota = document.createElement('div');
+        areaNota.className = 'areaNota';
+
+        const nota = document.createElement('div');
+        nota.className = 'nota';
+
+        const valorNota = document.createElement('span');
+        valorNota.className = 'valor';
+        valorNota.innerText = notas[i];
+        nota.appendChild(valorNota);
+
+        const barraNota = document.createElement('div');
+        barraNota.className = 'barra';
+        nota.appendChild(barraNota);
+
+        areaNota.appendChild(nota);
+
+        const nomeMateria = document.createElement('span');
+        nomeMateria.className = 'materia';
+        nomeMateria.innerText = materias[i];
+
+        areaNota.appendChild(nomeMateria);
+
+        grafico.appendChild(areaNota);
+    }
+}
+
+criandoGrafico()
 
 const notas = document.querySelectorAll('.valor');
 const barras = document.querySelectorAll('.barra');
