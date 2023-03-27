832c82239b8b01e9322c1ae4b0e468461201f015
unknown <biancapeleao@gmail.com>
2023-03-27 19:39:15 -0300

    finalizei filter

--- js/second.js
+++ js/second.js
@@ -15,20 +15,29 @@ const containerCards = document.getElementById('containerCard')
 const nomeCurso = document.getElementById('cursoNome')
 let alunoQueFinalizou;
 let arrayFinalizados = []
+let alunoCursante;
+let arrayCursante = []
 const aluno = await pesquisarAlunoCurso(sigla)
 //console.log(aluno.alunos[0].nome)
 
-const alunoStatus = () => {
+const alunoStatusFinalizado = () => {
     statusFinalizado.alunos.forEach(function (nomeFinalizado) {
         alunoQueFinalizou = nomeFinalizado.nome
         arrayFinalizados.push(alunoQueFinalizou)
     })
     return arrayFinalizados
 }
+const alunoStatusCursando = () => {
+    statusCursando.alunos.forEach(function (nomeCursando) {
+        alunoCursante= nomeCursando.nome
+        arrayCursante.push(alunoCursante)
+    })
+    return arrayCursante
+}
 
-console.log(arrayFinalizados)
 
-alunoStatus()
+alunoStatusFinalizado()
+alunoStatusCursando()
 
 const criandoCards = () => {
 
@@ -63,10 +72,100 @@ const criandoCards = () => {
         }
     })
 
-
-
 }
 
 criandoCards()
+
+
+const criandoCardsFinalizado = () => {
+
+    cursosLista.cursos.forEach(function (curso) {
+        if (curso.sigla == sigla) {
+            nomeCurso.innerHTML = curso.nome.slice(6)
+        }
+    });
+
+    aluno.alunos.forEach(function (aluno) {
+        if(arrayFinalizados.includes(aluno.nome)){
+            const card = document.createElement('div')
+            card.classList.add('card')
+            const img = document.createElement('img')
+            img.src = aluno.foto
+            img.classList.add('imgFormat')
+            const btn = document.createElement('button')
+            btn.classList.add('btnFormat')
+            const a = document.createElement('a')
+            a.href = './aluno.html'
+            a.innerHTML = aluno.nome
+    
+            btn.addEventListener('click', () => { localStorage.setItem('nome', a.textContent) })
+    
+            containerCards.append(card)
+            card.append(img, btn)
+            btn.append(a)
+    
+            if (arrayFinalizados.includes(aluno.nome)){
+                card.classList.add('bgFinalizado')
+            }else{
+                card.classList.add('bgEstudando')
+            }
+        }
+       
+    })
+
+}
+const criandoCardsCursante = () => {
+
+    cursosLista.cursos.forEach(function (curso) {
+        if (curso.sigla == sigla) {
+            nomeCurso.innerHTML = curso.nome.slice(6)
+        }
+    });
+
+    aluno.alunos.forEach(function (aluno) {
+        if(arrayCursante.includes(aluno.nome)){
+            const card = document.createElement('div')
+            card.classList.add('card')
+            const img = document.createElement('img')
+            img.src = aluno.foto
+            img.classList.add('imgFormat')
+            const btn = document.createElement('button')
+            btn.classList.add('btnFormat')
+            const a = document.createElement('a')
+            a.href = './aluno.html'
+            a.innerHTML = aluno.nome
+    
+            btn.addEventListener('click', () => { localStorage.setItem('nome', a.textContent) })
+    
+            containerCards.append(card)
+            card.append(img, btn)
+            btn.append(a)
+    
+            if (arrayFinalizados.includes(aluno.nome)){
+                card.classList.add('bgFinalizado')
+            }else{
+                card.classList.add('bgEstudando')
+            }
+        }
+       
+    })
+
+}
+
+
+
+
+document.getElementById('finalizado').addEventListener('click', function() {
+    const container = document.getElementById('containerCard');
+    container.innerHTML = ''
+    criandoCardsFinalizado()
+  });
+  
+  document.getElementById('cursando').addEventListener('click', function() {
+    const container = document.getElementById('containerCard');
+    container.innerHTML = ''
+    criandoCardsCursante()
+  });
+  
 
 console.log('teste', nome)
