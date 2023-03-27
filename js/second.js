6b14d3031cba0a0f6c6fddd07d4242f922b200a4
unknown <biancapeleao@gmail.com>
2023-03-27 19:12:14 -0300

    verifica se está finalizado ou não ~ bianca

--- js/second.js
+++ js/second.js
@@ -2,16 +2,33 @@
 
 import { pesquisarCursos } from './api.js'
 import { pesquisarAlunoCurso } from './api.js'
+import { pesquisarAlunoStatus } from './api.js'
 
+const statusFinalizado = await pesquisarAlunoStatus('finalizado')
+const statusCursando = await pesquisarAlunoStatus('cursando')
+//console.log(statusFinalizado.alunos[0]);
 
 
 const sigla = localStorage.getItem('curso')
 const cursosLista = await pesquisarCursos()
 const containerCards = document.getElementById('containerCard')
 const nomeCurso = document.getElementById('cursoNome')
-
+let alunoQueFinalizou;
+let arrayFinalizados = []
 const aluno = await pesquisarAlunoCurso(sigla)
-console.log(aluno.alunos[0].nome)
+//console.log(aluno.alunos[0].nome)
+
+const alunoStatus = () => {
+    statusFinalizado.alunos.forEach(function (nomeFinalizado) {
+        alunoQueFinalizou = nomeFinalizado.nome
+        arrayFinalizados.push(alunoQueFinalizou)
+    })
+    return arrayFinalizados
+}
+
+console.log(arrayFinalizados)
+
+alunoStatus()
 
 const criandoCards = () => {
 
@@ -33,11 +50,17 @@ const criandoCards = () => {
         a.href = './aluno.html'
         a.innerHTML = aluno.nome
 
-        btn.addEventListener('click', () => {localStorage.setItem('nome', a.textContent)})
+        btn.addEventListener('click', () => { localStorage.setItem('nome', a.textContent) })
 
         containerCards.append(card)
         card.append(img, btn)
         btn.append(a)
+
+        if (arrayFinalizados.includes(aluno.nome)){
+            card.classList.add('bgFinalizado')
+        }else{
+            card.classList.add('bgEstudando')
+        }
     })
 
 
